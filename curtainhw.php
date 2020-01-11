d39ebaeebcaeee8b41e3cc3db7798a5a5b26ba6b
DESKTOP\Norm <norm.bosse0@gmail.com>
2020-01-11 11:52:30 -0500

    add google analytics tag

--- curtainhw.php
+++ curtainhw.php
@@ -10,6 +10,10 @@ and open the template in the editor.
 -->
 <html>
   <head>
+  <?php
+    require './includes/globalSiteTag.html';
+    ?>
+
     <title>Hardware</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- process.php
+++ process.php
@@ -11,7 +11,11 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Gates</title>
+    <?php
+    require './includes/globalSiteTag.html';
+    ?>
+
+        <title>Our Process</title>
         <meta charset="UTF-8">
         <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
         <link href="https://fonts.googleapis.com/css?family=IM+Fell+DW+Pica+SC" rel="stylesheet">
