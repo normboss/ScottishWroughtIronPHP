2da21cbe16293d1ce389576ab6ed56b79735a79b
DESKTOP\Norm <norm.bosse0@gmail.com>
2020-05-24 09:04:13 -0400

    titles and descriptions

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
 <?php
     require 'files/includes/globalSiteTag.html';
-    require 'files/includes/keywords.php';
 
     ?>
+    <title>Scottish Lion Wrought Iron | Custom forge</title>
+    <meta name="description" content="Shop gates,railings,hardware, sign brackets & garden art hand forged by Andy Leck">
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
